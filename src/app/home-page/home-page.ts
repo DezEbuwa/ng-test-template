e723dd86f1d4d0f13e33d41008b2cda94432b951
Dez Ebuwa <dezebuwa@gmail.com>
2025-06-27 09:59:16 -0400

    added home and styling

--- src/app/home-page/home-page.ts
+++ src/app/home-page/home-page.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [MatCardModule, MatButtonModule],
   templateUrl: './home-page.html',
   styleUrl: './home-page.css'
 })
